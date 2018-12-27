25ac026f0865c9b7b7c4292f6b3e1d2f19d12985
Gilbert Choy <gilbertchoy@gmail.com>
2018-12-26 23:02:31 -0800

    routing testing

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,20 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+bertnotes:
+-use regular expression constraints for safety
+
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function (){
+//$id is required
+Route::get('/test/{id}', function ($id) {
     return view('test');
+});
+
+Route::get('/teststring/{id}/comment/{comment}', function ($id, $comment) {
+    return 'User ' . $id . '  ' . $comment;
 });
